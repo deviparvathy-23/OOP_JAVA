d884b3b918b51ca9efb2d637caebfb596b665fca
S DEVI PARVATHY <deviparvathys2005@gmail.com>
2025-11-15 21:10:33 +0530

    Refactor matrix multiplication implementation

--- matrix_multiply.java
+++ matrix_multiply.java
@@ -1,50 +1,46 @@
 class Matrix
 {
-    int value[][]=new int[5][5];
-}
-public class Multiply
-{
-    public static void main(String args[])
+    int value[][]=new int[10][10];
+    int i,j,k;
+    Matrix multiply(Matrix n)
     {
-        int i,j,k;
-        Matrix m1,m2,m3;
-        m1=new Matrix();
-        m2=new Matrix();
-        m3=new Matrix();
-        int mat1[][]={{1,2,3},{4,5,6}};
-        int mat2[][]={{1,2},{3,4},{5,6}};
+        Matrix c=new Matrix ();
         for(i=0;i<2;i++)
-        {
-            for(j=0;j<3;j++)
-            {
-                m1.value[i][j]=mat1[i][j];
-            }
-        }
-        for(i=0;i<3;i++)
         {
             for(j=0;j<2;j++)
             {
-                m2.value[i][j]=mat2[i][j];
-            }
-        }
-        for(i=0;i<2;i++)
-        {
-            for(j=0;j<3;j++)
-            {
-                m3.value[i][j]=0;
+                c.value[i][j]=0;
                 for(k=0;k<3;k++)
                 {
-                    m3.value[i][j]+=m1.value[i][k]*m2.value[k][j];
+                    c.value[i][j]+=this.value[i][k]*n.value[k][j];
                 }
             }
         }
+        return c;
+    }
+}
+public class MatrixMultiplication
+{
+    public static void main(String[] args)
+    {
+        Matrix m1,m2,m4;
+        m1=new Matrix();
+        m2=new Matrix();
+        m4=new Matrix();
+        int a[][]={{1,2,3},{4,5,6}};
+        int b[][]={{1,2},{4,5},{6,2}};
+        m1.value=a;
+        m2.value=b;
+        m4= m1.multiply(m2);
+        int i,j;
         for(i=0;i<2;i++)
         {
             for(j=0;j<2;j++)
             {
-                System.out.print(m3.value[i][j]+" ");
+                System.out.print(m4.value[i][j]+" ");
             }
-            System.out.println();
+            System.out.println(" ");
         }
+
     }
 }
